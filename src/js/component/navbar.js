4871dbed53b7422846ecb10292c4336e1f9428b3
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-10-12 19:31:19 +0000

    corrected

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -42,12 +42,17 @@ export const Navbar = () => {
         </a>
         
         <ul className="dropdown-menu menu">
-          {store.favoritos.map(item => (
-            <li className="dropdown-item me-4"> {item.vehiculo?.name}{item.personaje?.name} {item.planeta?.name}{item.starship?.name}<span onClick={() => actions.eliminarFavorito(item)}><span className="ms-2 btn btn-danger float-end"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-            </svg></span></span></li>
-
-          ))}
+          {store.favoritos.map((item,index) => {
+            return(
+								<div  className="favorite-item">
+									<li>
+										<a class="dropdown-item" href="#">{item} 
+											<i className="fas fa-trash-alt trash" onClick={()=>actions.getDeleteFavorite(item)}></i>
+										</a>									
+									</li>
+								</div>
+								)
+                })}
         </ul>
       </div>
       {store.autenticar && <button type="submit"  class="btn btn-primary" onClick={handleLogout}>logout</button>}
